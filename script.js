51cc6c94bf99d61f640bd17e65367b38df552e85
Thiago <thidurante@hotmail.com>
2022-10-21 12:33:20 -0300

    Adds show current player and uppon player clicking adds X or O

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ console.log(squares)
 
 let player = document.querySelector('.player')
 
+//setting event listeners for all the game squares
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', () => {
-    console.log(i);
+    const player = document.querySelector('.player')
+    player.innerHTML === 'Player 1' 
+    ? player.innerHTML = 'Player 2' : player.innerHTML = 'Player 1'
+
+    if(player.innerHTML === 'Player 1') {
+      squares[i].innerHTML = 'X'
+    } else {
+      squares[i].innerHTML = 'O'
+    }
   })
 }
 
